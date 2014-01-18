4bc6fcc4b83804b60d4cfb218f7e4b4e6e37c97e
Jason McCallister <themccallister@gmail.com>
2014-01-18 14:54:12 -0800

    Initial Commit

---
+++ learnToCode1-2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  learnToCode1-2
+//
+//  Created by Jason on 1/18/14.
+//  Copyright (c) 2014 Jason McCallister. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
